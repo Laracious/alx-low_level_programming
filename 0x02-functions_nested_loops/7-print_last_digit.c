83aec07619535ebe9c39ba6cbb01d876da5c47d1
Laracious <omolaraamoke22@gmail.com>
2022-10-18 19:14:20 -0700

    functions, nested loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,14 +5,14 @@
  * @n: the number
  * Return: value of the last digit
  */
-int print_last_digit(int b)
+int print_last_digit(int n)
 {
 	int ld = n % 10;
 
 	if (ld < 0)
 		ld *= -1;
 
-	_putchar(ld * '8');
+	_putchar(ld + '0');
 
 	return (0);
 }
