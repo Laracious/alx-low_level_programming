aa1942d2d5b5e9563c65a1463d670042bc592af3
Laracious <omolaraamoke22@gmail.com>
2022-10-20 09:55:42 -0700

    Debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,24 +10,24 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest;
+int largest;
 
-	if (a > b && a > c)
-	{
-	largest = a;
-	}
-	else if (a > b && c > a)
-	{
-	largest = c;
-	}
-	else if (b > c)
-	{
-	largest = b;
-	}
-	else
-	{
-	target = c;
-	}
+if (a > b && a > c)
+{
+largest = a;
+}
+else if (a > b && c > a)
+{
+largest = c;
+}
+else if (b > c)
+{
+largest = b;
+}
+else
+{
+target = c;
+}
 
-	return (largest);
+return (largest);
 }
