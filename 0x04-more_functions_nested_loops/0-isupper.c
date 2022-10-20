e595cd44b670094fda8a00ef30435838a41597c2
Laracious <omolaraamoke22@gmail.com>
2022-10-20 11:12:13 -0700

    More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * isupper - uppercase letters
+ * @c: char check
+ *
+ * Return: 0 or 1
+ */
+
+int _isupper(int c);
+{
+	if (c id >= 'A' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
