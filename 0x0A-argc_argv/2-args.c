618428b117234f9b27675d93e149734c42936f3d
Laracious <omolaraamoke22@gmail.com>
2022-11-04 12:17:28 -0700

    argc and argv

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -5,13 +5,22 @@
  * main - print the name of the program
  * @argc: Count arguments
  * @argv: Arguments
- * Return: Always 0
+ * Return: always 0
  */
 
 int main(int argc, char *argv[])
 {
-	(void) argv; /*ignore argv*/
-	printf("%f\n", argc - 1);
+/*Declaring variable*/
+	int count = 0;
 
+	if (argc > 0)
+	{
+/*WHILE - prints each arguments*/
+	while (count < argc)
+	{
+		printf("%s\n", argv[count]);
+		count++;
+	}
+	}
 	return (0);
 }
