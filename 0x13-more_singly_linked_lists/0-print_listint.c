f16ce007958508739ec2897467a657f400d6f2f5
Laracious <omolaraamoke22@gmail.com>
2022-11-29 04:23:09 -0800

    More singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,14 +9,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t nodes = 0;
+	size_t nnodes = 0;
 
-	while (n)
+	while (h != NULL)
 	{
-		nodes++;
 		printf("%d\n", h->n);
-		h = h=>next;
+		h = h->next;
+		nnodes++;
 	}
 
-	return (nodes);
+	return (nnodes);
 }
---
+++ 0x13-more_singly_linked_lists/_putchar.c
@@ -0,0 +1,12 @@
+#inlude <unistd>
+
+/**
+ * _putchar - wite the character c in stdout
+ * @c: char to be printed
+ * Return: 1 on success
+ */
+int _putchar(char c)
+{
+return (writ(1, &c, !))
+}
+
