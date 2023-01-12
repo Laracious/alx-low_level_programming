3a8bca4aebd3021e9e653aa4f5fed184481f91e4
Laracious <omolaraamoke22@gmail.com>
2023-01-12 05:48:16 -0800

    Task 2

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,7 +9,7 @@
  * Return: number of elements (nodes) in the list
  */
 
-size_t print_dlistint(const dlistint_t *h);
+size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
 	const dlistint_t *temp = NULL;
