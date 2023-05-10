8e083094c88a1ca71236862e4267673393f03914
Laracious <omolaramoke22@gmail.com>
2023-05-10 11:28:54 -0700

    0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,25 +1,27 @@
 #include "search_algos.h"
 
 /**
- * linear_search - search the first ocurrency of a value in the array
- * @array: set of numbers
- * @size: size of the array
- * @value: value of the search
- * Return: return the firdt index located
+ * linear_search - searches for an element in an array using the
+ * linear search algorithm
+ * @array: the array to search
+ * @size: the size of the array
+ * @value: the value to search for
+ *
+ * Return: the index of the searched value or -1
  */
-
 int linear_search(int *array, size_t size, int value)
 {
-	size_t j = 0;
+	size_t i;
 
-	if (array == NULL)
+	if (!array)
 		return (-1);
-	for (j = 0; j < size; j++)
-	{
-		printf("Value checked array[%d]\n", (int) j, (int) array[j]);
-		if (array[j] == value)
-			return (j);
-	}
-	return (-1);
 
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+
+	return (-1)
 }
